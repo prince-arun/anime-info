e73b2f454fa4b8f89cdef0ed89a4609c3e64f326
prince-arun <prince.skillsafari@gmail.com>
2023-07-21 20:48:20 +0530

    Airing component finished

--- anime-info/src/components/Airing.js
+++ anime-info/src/components/Airing.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { AnimeContext } from "../context/context";
+import Sidebar from "./Sidebar";
 
-const Airing = () => {
-  return <div>Airing</div>;
+const Airing = ({ getPopular }) => {
+  const { airingAnime, isSearch, searchResults } = AnimeContext();
+
+  const showAiring = () => {
+    if (!isSearch && getPopular === "airing") {
+      return airingAnime?.map((anime) => {
+        return (
+          <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+            <img src={anime.images.jpg.large_image_url} alt="" />
+          </Link>
+        );
+      });
+    } else {
+      return searchResults?.map((anime) => {
+        return (
+          <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+            <img src={anime.images.jpg.large_image_url} alt="" />
+          </Link>
+        );
+      });
+    }
+  };
+  return (
+    <div className="airing">
+      <div className="airing-anime">{showAiring()}</div>
+      <Sidebar />
+    </div>
+  );
 };
 
 export default Airing;
