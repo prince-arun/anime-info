d33aa9e468a675591aef7db6ab75ff1d5b4b2732
prince-arun <prince.skillsafari@gmail.com>
2023-07-21 20:52:52 +0530

    Home page component started

--- anime-info/src/components/Homepage.js
+++ anime-info/src/components/Homepage.js
@@ -1,7 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
+import { AnimeContext } from "../context/global";
+import Popular from "./Popular";
+import Upcoming from "./Upcoming";
+import Airing from "./Airing";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  const {
+    handleSubmit,
+    search,
+    searchAnime,
+    handleChange,
+    getUpcomingAnime,
+    getAiringAnime,
+    getPopularAnime,
+  } = AnimeContext();
+
+  const [getPopular, setRendered] = useState("popular");
+
+  const switchComponent = () => {
+    switch (getPopular) {
+      case "popular":
+        return <Popular getPopular={getPopular} />;
+      case "airing":
+        return <Airing getPopular={getPopular} />;
+      case "upcoming":
+        return <Upcoming getPopular={getPopular} />;
+      default:
+        return <Popular getPopular={getPopular} />;
+    }
+  };
+  return (
+    <div className="homepage">
+      <header>
+        <div className="logo">
+          <h1>
+            {getPopular === "popular"
+              ? "Popular Anime"
+              : getPopular === "airing"
+              ? "Airing Anime"
+              : "Upcoming Anime"}
+          </h1>
+        </div>
+        <div className="search-container">
+          <div className="filter-btn popular-filter">
+            <button
+              onClick={() => {
+                setRendered("popular");
+              }}
+            >
+              Popular<i className="fas fa-fire"></i>
+            </button>
+          </div>
+          <form action="" className="search-form" onSubmit={handleSubmit}>
+            <div className="input-control">
+              <input
+                type="text"
+                placeholder="Search Anime"
+                value={search}
+                onChange={handleChange}
+              />
+              <button type="submit">Search</button>
+            </div>
+          </form>
+          <div className="filter-btn airing-filter">
+            <button
+              onClick={() => {
+                setRendered("airing");
+                getAiringAnime();
+              }}
+            >
+              Airing
+            </button>
+          </div>
+          <div className="filter-btn upcoming-filter">
+            <button
+              onClick={() => {
+                setRendered("upcoming");
+                getUpcomingAnime();
+              }}
+            >
+              Upcoming
+            </button>
+          </div>
+        </div>
+      </header>
+      {switchComponent()}
+    </div>
+  );
 };
 
 export default Homepage;
