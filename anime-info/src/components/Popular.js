3e9ec154e85bfc6290ec460cebac3ac1f02fd86d
prince-arun <prince.skillsafari@gmail.com>
2023-07-21 20:08:48 +0530

    Popular component finished

--- anime-info/src/components/Popular.js
+++ anime-info/src/components/Popular.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { AnimeContext } from "../context/global";
+import Sidebar from "./Sidebar";
 
-const Popular = () => {
-  return <div>Popular</div>;
+const Popular = ({ getPopular }) => {
+  const { popularAnime, isSearch, searchResults } = AnimeContext();
+
+  const displayPopular = () => {
+    if (!isSearch && getPopular === "popular") {
+      return popularAnime?.map((anime) => {
+        return (
+          <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+            <img src={anime.images.jpg.large_image_url} alt="" />
+          </Link>
+        );
+      });
+    } else {
+      return searchResults?.map((anime) => {
+        return (
+          <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+            <img src={anime.images.jpg.large_image_url} alt="" />
+          </Link>
+        );
+      });
+    }
+  };
+
+  return (
+    <div className="pop">
+      <div className="popular-anime">{displayPopular()}</div>
+      <Sidebar />
+    </div>
+  );
 };
 
 export default Popular;
