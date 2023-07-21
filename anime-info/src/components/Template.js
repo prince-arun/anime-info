0a76339da7094e901ac3261b7bfe50bac00b0c3d
prince-arun <prince.skillsafari@gmail.com>
2023-07-21 20:21:26 +0530

    Template component finished

--- anime-info/src/components/Template.js
+++ anime-info/src/components/Template.js
@@ -1,7 +1,60 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { AnimeContext } from "../context/context";
 
 const Template = () => {
-  return <div>Template</div>;
+  const { getAnimePictures, pictures } = AnimeContext();
+  const { id } = useParams();
+
+  //state
+  const [index, setIndex] = useState(0);
+
+  const handleImageClick = (i) => {
+    setIndex(i);
+  };
+
+  useEffect(() => {
+    getAnimePictures(id);
+  }, [id]);
+  return (
+    <div className="gallery">
+      <div className="back">
+        <Link to="/">
+          <i className="fas fa-arrow-left"></i>
+          Back to Home
+        </Link>
+      </div>
+      <div className="big-image">
+        <img src={pictures[index]?.jpg.image_url} alt="" />
+      </div>
+      <div className="small-images">
+        {pictures?.map((picture, i) => {
+          return (
+            <div
+              className="image-con"
+              onClick={() => {
+                handleImageClick(i);
+              }}
+              key={i}
+            >
+              <img
+                src={picture?.jpg.image_url}
+                style={{
+                  border:
+                    i === index ? "3px solid #27AE60" : "3px solid #e5e7eb",
+                  filter: i === index ? "grayscale(0)" : "grayscale(60%)",
+                  transform: i === index ? "scale(1.1)" : "scale(1)",
+                  transition: "all .3s ease-in-out",
+                }}
+                alt=""
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Template;
