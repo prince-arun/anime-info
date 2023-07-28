0f6e56613cca4576d7b1c843bceb71b2876c81ee
prince-arun <prince.skillsafari@gmail.com>
2023-07-28 19:27:41 +0530

    Bootstrap installed

--- anime-info/src/App.js
+++ anime-info/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Homepage from "./components/Homepage";
 import SingleItem from "./components/SingleItem";
 import Template from "./components/Template";
